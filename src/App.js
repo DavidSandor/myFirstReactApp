f857684699c1616bb7acac6a860b91b72bb05575
DavidSandor <sandor.david89@gmail.com>
2019-03-18 22:21:40 +0100

    Person update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+state = {
+  persons: [
+    {name: "David", age: 29},
+    {name: "Bob", age: 34}
+  ]
+}
+
+nameChangeHandler = () => {
+  this.setState({
+    persons: [
+      {name: "Dave", age: 29},
+      {name: "Bob", age: 34}
+    ]
+  })
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={this.nameChangeHandler}>Change Name</button>                
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         </header>
       </div>
     );
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+
+const person = (props) => {
+    return <p>My name is {props.name} and I am {props.age} years old!</p>;
+    
+};
+
+export default person;

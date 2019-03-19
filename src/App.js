135f9168988db3694b7ee3ad34313cb1c4408f0e
DavidSandor <sandor.david89@gmail.com>
2019-03-19 21:43:44 +0100

    Person state update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ state = {
   persons: [
     {name: "David", age: 29},
     {name: "Bob", age: 34}
-  ]
+  ],
+  hobby: "skiing"
 }
 
 nameChangeHandler = () => {
@@ -27,7 +28,7 @@ nameChangeHandler = () => {
           <img src={logo} className="App-logo" alt="logo" />
           <button onClick={this.nameChangeHandler}>Change Name</button>                
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> And I like {this.state.hobby}</Person>
         </header>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 
 const person = (props) => {
-    return <p>My name is {props.name} and I am {props.age} years old!</p>;
-    
+    return <p>My name is {props.name} and I am {props.age} years old! {props.children}</p>;
 };
 
 export default person;
